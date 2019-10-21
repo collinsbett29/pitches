18f193ff856262195db4123a20edaf0fca28406e
collinsbett29 <collinsbett29@gmail.com>
2019-10-21 18:46:07 +0300

    Test the application

---
+++ tests/pitch_test.py
@@ -0,0 +1,14 @@
+import unittest
+from models import pitch
+Pitch = pitch.Pitch
+
+class PitchTest(unittest.TestCase):
+    '''
+    Test Class t test the behaviour of the pitch class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        
