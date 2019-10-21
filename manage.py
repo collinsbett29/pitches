ff46edb6cf9922e2573004295dc7b46430297f5f
collinsbett29 <collinsbett29@gmail.com>
2019-10-21 18:31:05 +0300

    Add a decorator

---
+++ manage.py
@@ -0,0 +1,19 @@
+from app import create_app,db
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager,Server
+from app.models import User
+
+# creating an app instance
+app = create_app('development')
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Pitch = Pitch, category = category, Vote = Vote, Comment = Comment)
+if __name__=='__main__':
+    manager.run()
