def19ca21f5183439c691cac9f218fe5a955d7cc
collinsbett29 <collinsbett29@gmail.com>
2019-10-21 18:22:30 +0300

    Create init.py template

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,32 @@
 from flask import Flask
-from flask_bootsrap import Bootstrap
+from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
 
     app = Flask(__name__)
 
-    # # setting the config
-    # app.config.from_object(DevConfig)
-    
+    # Initializing application
+    app = Flask(__name__,instance_relative_config= True)
+
+
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
     # Initializing flask Extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # Registering the blueprint
-    from .main import main as main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # setting the config
+    from .requests import configure_request
+    configure_request(app)
 
     return app
