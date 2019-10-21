20c01d3c0570d50dcb865af1c5aaf02a23237d60
collinsbett29 <collinsbett29@gmail.com>
2019-10-21 18:29:45 +0300

    Add a secret key

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:##@@collins1@localhost/pitch'
 
 class ProdConfig(Config):    
     pass
