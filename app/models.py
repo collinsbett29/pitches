9df003f5cf1690669f371bc9baa5cb0b920ef331
collinsbett29 <collinsbett29@gmail.com>
2019-10-22 20:46:24 +0300

    Add a comment class

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from . import db
-# from . import db, login_manager
+from . import  login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
-# from flask_login import UserMixin
+from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -10,8 +10,8 @@ def load_user(user_id):
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
     pitches = db.relationship('Pitch', backref = 'user', lazy = "dynamic")
     comments = db.relationship('Comment', backref = 'user', lazy = "dynamic")
