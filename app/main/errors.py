9884283e8ad5f799f6b9b695d1d8b26a15024173
collinsbett29 <collinsbett29@gmail.com>
2019-10-24 14:53:37 +0300

    Return a 404 error if user enters invalid page

--- app/main/errors.py
+++ app/main/errors.py
@@ -6,4 +6,4 @@ def four_Ow_four(error):
     '''
     Function to render the 404 error page
     '''
-    return render_template('four_Ow_four.html'),404
+    return render_template('fourOwfour.html'),404
