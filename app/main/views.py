8e66bb6ece20cfb612854493ebb24238d2497e48
collinsbett29 <collinsbett29@gmail.com>
2019-10-24 23:11:13 +0300

    Update profile picture when changed

--- app/main/views.py
+++ app/main/views.py
@@ -97,28 +97,13 @@ def profile(uname):
     return render_template("profile/profile.html", user = user)
 
 
-@main.route('/user/<uname>/update',methods = ['GET','POST'])
-def update_profile(uname):
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
     user = User.query.filter_by(username = uname).first()
-    if user is None:
-        abort(404)
-
-    form = UpdateProfile()
-
-    if form.validate_on_submit():
-        user.bio = form.bio.data
-
-        db.session.add(user)
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
         db.session.commit()
-
-        return redirect(url_for('.profile',uname=user.username))
-
-    return render_template('profile/update.html',form =form)
-
-# @main.route('/comment/<int:id>')
-# def single_comment(id):
-#     comment=Comment.query.get(id)
-#     if comment is None:
-#         abort(404)
-#     format_comment = markdown2.markdown(comment.pitch_comment,extras=["code-friendly", "fenced-code-blocks"]) 
-#     return render_template('comment.html'comment = comment,format_comment=format_comment)   
+    return redirect(url_for('main.profile',uname=uname))
