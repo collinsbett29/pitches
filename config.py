847ef541db80e8c678ab936b0f33c1a4f81f0228
collinsbett29 <collinsbett29@gmail.com>
2019-10-24 23:25:46 +0300

    Validate a send message functionality

--- config.py
+++ config.py
@@ -1,18 +1,21 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:##@@collins1@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-
-# email comfigurations
+    
+ # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
+#  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL__PASSWORD = os.environ.get("MAIL_PASSWORD")
-
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):    
     pass
