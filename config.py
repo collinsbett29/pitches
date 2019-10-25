2bd6889f6c6cff306d727ef2f93b42854f0b511b
collinsbett29 <collinsbett29@gmail.com>
2019-10-25 08:53:10 +0300

    create a welcome message for the user

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:##@@collins1@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
  # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
